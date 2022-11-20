fe5e7a62e62ec8bf0547d47c08171aca99865749
Michael Nguyen <michaelnguyen.creative@gmail.com>
2022-11-20 19:08:45 +0700

    Added list helpers & unit tests & 4.5 done

--- part4/bloglist/utils/list_helpers.js
+++ part4/bloglist/utils/list_helpers.js
@@ -1,29 +1,16 @@
-const dummy = (blogsArr) => 1
+const dummy = () => 1
 
 const totalLikes = (blogListArr) => blogListArr.reduce((acc, { likes }) => acc + likes, 0)
 
 const favoriteBlog = (blogListArr) => {
-    const maxLike = blogListArr.reduce((acc, blog) => {
-        if (blog.likes >= acc) {
-            acc = blog.likes
-        }
-        return acc
-    }, 0)
+  const maxLike = blogListArr.reduce((acc, blog) => {
+    if (blog.likes >= acc) {
+      acc = blog.likes
+    }
+    return acc
+  }, 0)
 
-    return blogListArr.find((blog) => blog.likes === maxLike)
+  return blogListArr.find((blog) => blog.likes === maxLike)
 }
-
-// console.log(blogListArr.filter(({ likes }, index, array) => {
-//     const max = array.reduce((acc, { curr }) => {
-//         while (curr < acc) {
-//             acc++
-//         } else {
-//             acc++
-//         }
-//         return acc
-//     }, 0)
-
-//     likes === max
-// }))
 
 module.exports = { dummy, totalLikes, favoriteBlog }
