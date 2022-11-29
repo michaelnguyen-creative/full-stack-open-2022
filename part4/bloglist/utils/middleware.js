1b5ee9761c4fa9ba1b75377b8803fbe07d189db4
Michael Nguyen <michaelnguyen.creative@gmail.com>
2022-11-29 13:04:26 +0700

    added comment

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -8,6 +8,7 @@ const tokenExtractor = (req, res, next) => {
   const authz = req.get('authorization')
   // console.log('authz', authz)
   if (authz && authz.toLowerCase().startsWith('bearer')) {
+    // This is how you add a new field/property to a JS object
     req.token = authz.substring(7)
   } else {
     req.token = null
