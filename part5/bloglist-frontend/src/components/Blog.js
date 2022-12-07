f708d24630046053d35823883ff0201b0154b1db
Michael Nguyen <michaelnguyen.creative@gmail.com>
2022-12-07 11:46:41 +0700

    implemented show blog details

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,38 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetail, setShowDetail] = useState(false)
+  const { url, likes, title, author, user } = blog
+  
+  const showAllDetail = {
+    display: showDetail ? 'flex' : 'none',
+    flexDirection: 'column',
+    border: '1px solid black',
+    padding: '10px',
+    margin: ' 5px 0 5px 0',
+  }
+  const hideAllDetail = { display: showDetail ? 'none' : '' }
+
+  return (
+    <div>
+      {!showDetail ? (
+        <div>
+          {title} {author} 
+          <button onClick={() => setShowDetail(true)}>view</button>
+        </div>
+      ) : (
+        <div style={showDetail ? showAllDetail : hideAllDetail}>
+          <div>
+            {title} {author} 
+            <button onClick={() => setShowDetail(false)}>hide</button>
+          </div>
+          <div>{url}</div>
+          <div>{`likes: ${likes}`}</div>
+          <div>{user !== undefined && user.name}</div>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
