fb700d38f46e0705c42f0027fb509176b89adbb2
Michael Nguyen <michaelnguyen.creative@gmail.com>
2022-12-27 21:54:42 +0700

    add reset input field value feature

--- part7/ultimate-hooks/src/App.js
+++ part7/ultimate-hooks/src/App.js
@@ -41,9 +41,9 @@ const useResource = (baseUrl) => {
 }
 
 const App = () => {
-  const content = useField('text')
-  const name = useField('text')
-  const number = useField('text')
+  const {reset: resetContent, ...content} = useField('text')
+  const {reset: resetName, ...name} = useField('text')
+  const {reset: resetNumber, ...number} = useField('text')
 
   const [notes, noteService] = useResource('http://localhost:3005/notes')
   const [persons, personService] = useResource('http://localhost:3005/persons')
@@ -51,11 +51,14 @@ const App = () => {
   const handleNoteSubmit = (event) => {
     event.preventDefault()
     noteService.create({ content: content.value })
+    resetContent()
   }
 
   const handlePersonSubmit = (event) => {
     event.preventDefault()
     personService.create({ name: name.value, number: number.value })
+    resetName()
+    resetNumber()
   }
 
   return (
