80a4ddde5a56d0941f8a446f3d53423715943cc0
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-01-05 13:57:25 +0700

    configure a simple redux store

--- part7/bloglist-extension/src/index.js
+++ part7/bloglist-extension/src/index.js
@@ -1,5 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const blogReducer = createSlice({
+  name: 'blogs',
+  initialState: [],
+  reducers: {
+    addNew(state, action) {
+      state.push(action.payload)
+    }
+  }
+})
+
+const store = configureStore({
+  reducer: blogReducer
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
