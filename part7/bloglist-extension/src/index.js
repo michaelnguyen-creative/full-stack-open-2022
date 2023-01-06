64498fa8a5513b844ce9ec47958b7a0569b8a957
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-01-06 13:35:41 +0700

    refactor redux store into its own file

--- part7/bloglist-extension/src/index.js
+++ part7/bloglist-extension/src/index.js
@@ -1,19 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
-import blogReducer from './reducers/blogReducer'
-import notifReducer from './reducers/notifReducer'
-import userReducer from './reducers/userReducer'
-
-const store = configureStore({
-  reducer: {
-    blogs: blogReducer,
-    message: notifReducer,
-    user: userReducer
-  }
-})
+import store from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
---
+++ part7/bloglist-extension/src/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit'
+import blogReducer from './reducers/blogReducer'
+import notifReducer from './reducers/notifReducer'
+import userReducer from './reducers/userReducer'
+
+const store = configureStore({
+  reducer: {
+    blogs: blogReducer,
+    message: notifReducer,
+    user: userReducer
+  }
+})
+
+export default store
