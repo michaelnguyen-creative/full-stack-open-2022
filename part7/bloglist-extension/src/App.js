3c252ae503be3797f20f3650ddfd8b7e6ec8b256
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-01-07 19:15:35 +0700

    refactor users view into its own module

--- part7/bloglist-extension/src/App.js
+++ part7/bloglist-extension/src/App.js
@@ -15,6 +15,7 @@ import {
 import { logIn, logOut } from './reducers/userReducer'
 import { BrowserRouter, Link } from 'react-router-dom'
 import { Routes, Route } from 'react-router'
+import UsersView from './components/UsersView'
 import SingleUserView from './components/SingleUserView'
 
 const App = () => {
@@ -22,7 +23,6 @@ const App = () => {
   const message = useSelector(({ message }) => message)
   const user = useSelector(({ user }) => user)
   const dispatch = useDispatch()
-
 
   useEffect(() => {
     // Clear localStorage data if there is any
@@ -49,45 +49,6 @@ const App = () => {
 
   const deleteBlogById = async (blogId) => {
     dispatch(deleteBlog(blogId))
-  }
-
-  const renderUsers = (blogs) => {
-    const usersWithBlogCounts = blogs.reduce((acc, { user }) => {
-      const currCount = acc[[user.name, user.userId]] ?? 0
-      return { ...acc, [[user.name, user.userId]]: currCount + 1 }
-    }, {})
-
-    const usersArrayFromObj = Object.entries(usersWithBlogCounts)
-    const users = usersArrayFromObj.map((user) => [
-      ...user[0].split(','),
-      user[1],
-    ])
-
-    return (
-      <>
-        <div>
-          <h1>Users</h1>
-          <table>
-            <thead>
-              <tr>
-                <th>name</th>
-                <th>blogs created</th>
-              </tr>
-            </thead>
-            <tbody>
-              {users.map((user) => (
-                <tr key={user[1]}>
-                  <td>
-                    <Link to={user[1]}>{user[0]}</Link>
-                  </td>
-                  <td>{user[2]}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </>
-    )
   }
 
   return (
@@ -128,14 +89,19 @@ const App = () => {
             path="/users"
             element={
               <>
-                <UserBlogs user={user} handleLogout={handleLogout} />
-                {renderUsers(blogs)}
+                <UserBlogs handleLogout={handleLogout} />
+                <UsersView />
               </>
             }
           />
           <Route
             path="/users/:userId"
-            element={<SingleUserView />}
+            element={
+              <>
+                <UserBlogs handleLogout={handleLogout} />
+                <SingleUserView />
+              </>
+            }
           />
         </Routes>
       </BrowserRouter>
---
+++ part7/bloglist-extension/src/components/UsersView.js
@@ -0,0 +1,47 @@
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+const UsersView = () => {
+  const blogs = useSelector(({ blogs }) => blogs)
+
+  const usersWithBlogCounts = blogs.reduce((acc, { user }) => {
+    const currCount = acc[[user.name, user.userId]]
+      ? acc[[user.name, user.userId]]
+      : 0
+    return { ...acc, [[user.name, user.userId]]: currCount + 1 }
+  }, {})
+
+  const usersArrayFromObj = Object.entries(usersWithBlogCounts)
+  const users = usersArrayFromObj.map((user) => [
+    ...user[0].split(','),
+    user[1],
+  ])
+
+  return (
+    <>
+      <div>
+        <h1>Users</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>name</th>
+              <th>blogs created</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user[1]}>
+                <td>
+                  <Link to={user[1]}>{user[0]}</Link>
+                </td>
+                <td>{user[2]}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  )
+}
+
+export default UsersView
