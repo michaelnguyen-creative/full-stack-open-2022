57f0a33f94ccbcbcec2728cbf0f68ca78c578746
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-01-12 22:58:05 +0700

    modify allBooks query to incude author para

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -97,7 +97,7 @@ const typeDefs = gql`
   type Query {
     authorCount: Int!
     bookCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String!): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -106,7 +106,7 @@ const resolvers = {
   Query: {
     authorCount: () => authors.length,
     bookCount: () => books.length,
-    allBooks: () => books,
+    allBooks: (root, args) => books.filter((b) => b.author === args.author),
     allAuthors: () => {
       return authors.map((a) => {
         return {
