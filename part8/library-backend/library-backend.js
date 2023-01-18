59852d265091517f965ced0aabb6c9ede89ff53d
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-01-18 15:54:44 +0700

    add mongoDB & refactor some queries and schemas

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -1,83 +1,17 @@
-const { ApolloServer, gql } = require('apollo-server')
-const { v1: uuidv1 } = require('uuid')
+import { ApolloServer, gql } from 'apollo-server'
+import mongoose from 'mongoose'
+import Book from './models/book.js'
+import Author from './models/author.js'
+import { v1 as uuidv1 } from 'uuid'
 
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: 'afa51ab0-344d-11e9-a414-719c6709cf3e',
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: 'afa5b6f0-344d-11e9-a414-719c6709cf3e',
-    born: 1963,
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: 'afa5b6f1-344d-11e9-a414-719c6709cf3e',
-    born: 1821,
-  },
-  {
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: 'afa5b6f2-344d-11e9-a414-719c6709cf3e',
-  },
-  {
-    name: 'Sandi Metz', // birthyear not known
-    id: 'afa5b6f3-344d-11e9-a414-719c6709cf3e',
-  },
-]
+const MONGODB_URI = `mongodb+srv://michaelnguyen-creative:lM6g7yltO01zaeqR@cluster0.9tpxnaf.mongodb.net/libraryApp?retryWrites=true&w=majority`
 
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: 'afa5b6f4-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring'],
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: 'afa5b6f5-344d-11e9-a414-719c6709cf3e',
-    genres: ['agile', 'patterns', 'design'],
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: 'afa5de00-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring'],
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: 'afa5de01-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring', 'patterns'],
-  },
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: 'afa5de02-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring', 'design'],
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: 'afa5de03-344d-11e9-a414-719c6709cf3e',
-    genres: ['classic', 'crime'],
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: 'afa5de04-344d-11e9-a414-719c6709cf3e',
-    genres: ['classic', 'revolution'],
-  },
-]
+console.log('connecting to MongoDB at', MONGODB_URI)
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => console.log('connected to MongoDB'))
+  .catch((error) => console.log('error', error.message))
 
 const typeDefs = gql`
   type Author {
@@ -106,33 +40,35 @@ const typeDefs = gql`
     addBook(
       title: String!
       author: String!
-      published: String!
+      published: Int!
       genres: [String!]!
-    ): Book
+    ): Book!
     editAuthor(name: String!, born: Int!): Author
   }
 `
 
 const resolvers = {
   Query: {
-    authorCount: () => authors.length,
-    bookCount: () => books.length,
-    allBooks: (root, args) => {
+    authorCount: async () => Author.estimatedDocumentCount(),
+    bookCount: async () => Book.estimatedDocumentCount(),
+    allBooks: async (root, args) => {
+      // TODO
       if (Object.keys(args).length === 0) {
-        return books
+        return Book.find({})
+      } else if (args.author && args.genre) {
+        return Book.find({ author: args.author, genres: { $in: [args.genre] } })
+      } else {
+        return Book.find({ $or: [{ author: args.author }, { genres: { $in: [args.genre] } }] })
       }
-
-      return books.filter((b) => {
-        if (args.author && args.genre) {
-          return b.author === args.author && b.genres.includes(args.genre)
-        }
-        return b.author === args.author || b.genres.includes(args.genre)
-      })
     },
     allAuthors: () => authors
   },
   Author: {
     bookCount: (root) => books.filter((b) => b.author === root.name).length
+  },
+  Book: {
+    // TODO
+    author: (root) => console.log('root', root)
   },
   Mutation: {
     addBook: (root, args) => {
