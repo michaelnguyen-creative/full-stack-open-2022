b0104d0f401222b14bfd9feace43f90b21c896fb
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-02-06 14:36:07 +0700

    integrate backend & frontend

--- part9/patientor/backend/src/index.ts
+++ part9/patientor/backend/src/index.ts
@@ -1,6 +1,11 @@
 import express from "express";
+import cors from "cors";
+
 const app = express();
 const PORT = 4000;
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 
 app.get("/api/ping", (_req, res) => {
   res.send("pong");
