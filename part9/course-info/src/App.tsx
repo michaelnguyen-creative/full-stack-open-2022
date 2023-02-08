154be27eba37c29f1705973fb8cacc766588a166
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-02-08 20:05:51 +0700

    extend app to use TS types

--- part9/course-info/src/App.tsx
+++ part9/course-info/src/App.tsx
@@ -1,32 +1,149 @@
-interface Part {
+interface CoursePartBase {
   name: string
   exerciseCount: number
+  type: string
 }
 
-interface Course {
-  courseName?: string
-  courseParts: Array<Part>
+interface CoursePartDescription extends CoursePartBase {
+  description: string
 }
 
-const Header = ({ name }: { name: string }) => <h1>{name}</h1>
+interface CourseNormalPart extends CoursePartDescription {
+  type: 'normal'
+}
 
-const Content = ({ courseParts }: Course) => {
-  return (
-    <>
-      <p>
-        {courseParts[0].name} {courseParts[0].exerciseCount}
-      </p>
-      <p>
-        {courseParts[1].name} {courseParts[1].exerciseCount}
-      </p>
-      <p>
-        {courseParts[2].name} {courseParts[2].exerciseCount}
-      </p>
-    </>
+interface CourseProjectPart extends CoursePartBase {
+  type: 'groupProject'
+  groupProjectCount: number
+}
+
+interface CourseSubmissionPart extends CoursePartDescription {
+  type: 'submission'
+  exerciseSubmissionLink: string
+}
+
+interface CourseSpecialPart extends CoursePartDescription {
+  type: 'special'
+  requirements: Array<string>
+}
+
+type CoursePart =
+  | CourseNormalPart
+  | CourseProjectPart
+  | CourseSubmissionPart
+  | CourseSpecialPart
+
+const courseParts: CoursePart[] = [
+  {
+    name: 'Fundamentals',
+    exerciseCount: 10,
+    description: 'This is the easy course part',
+    type: 'normal',
+  },
+  {
+    name: 'Advanced',
+    exerciseCount: 7,
+    description: 'This is the hard course part',
+    type: 'normal',
+  },
+  {
+    name: 'Using props to pass data',
+    exerciseCount: 7,
+    groupProjectCount: 3,
+    type: 'groupProject',
+  },
+  {
+    name: 'Deeper type usage',
+    exerciseCount: 14,
+    description: 'Confusing description',
+    exerciseSubmissionLink: 'https://fake-exercise-submit.made-up-url.dev',
+    type: 'submission',
+  },
+  {
+    name: 'Backend development',
+    exerciseCount: 21,
+    description: 'Typing the backend',
+    requirements: ['nodejs', 'jest'],
+    type: 'special',
+  },
+]
+
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
   )
 }
 
-const Total = ({ courseParts }: Course): JSX.Element => {
+courseParts.forEach((p) => {
+  switch (p.type) {
+    case 'normal':
+      break
+    case 'groupProject':
+      break
+    case 'submission':
+      break
+    case 'special':
+      break
+    default:
+      assertNever(p)
+  }
+})
+
+interface PartProps {
+  part: CoursePart
+  key?: string
+}
+
+const Part = ({ part }: PartProps): JSX.Element => {
+  const { name, exerciseCount, ...attr } = part
+  return (
+    <div>
+      <h3>
+        {name} {exerciseCount}
+      </h3>
+      {attr.type === 'normal' ? (
+        <p>{attr.description}</p>
+      ) : attr.type === 'groupProject' ? (
+        <p>project exercises {attr.groupProjectCount}</p>
+      ) : attr.type === 'submission' ? (
+        <>
+          <p>{attr.description}</p>
+          <p>submit to {attr.exerciseSubmissionLink}</p>
+        </>
+      ) : attr.type === 'special' ? (
+        <>
+          <p>{attr.description}</p>
+          <p>required skills: {attr.requirements.join(', ')}</p>
+        </>
+      )
+        : (
+        ''
+      )}
+    </div>
+  )
+}
+
+type HeaderProps = {
+  courseName: string
+}
+
+const Header = ({ courseName }: HeaderProps) => <h1>{courseName}</h1>
+
+type CoursePartsProps = {
+  courseParts: Array<CoursePart>
+}
+
+const Content = ({ courseParts }: CoursePartsProps) => {
+  return (
+    <div>
+      {courseParts.map((p) => (
+        <Part key={p.name} part={p} />
+      ))}
+    </div>
+  )
+}
+
+const Total = ({ courseParts }: CoursePartsProps): JSX.Element => {
   return (
     <p>
       Number of exercises{' '}
@@ -34,26 +151,13 @@ const Total = ({ courseParts }: Course): JSX.Element => {
     </p>
   )
 }
+
 const App = () => {
   const courseName = 'Half Stack app dev'
-  const courseParts = [
-    {
-      name: 'Fundamentals',
-      exerciseCount: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exerciseCount: 7,
-    },
-    {
-      name: 'Deeper type usage',
-      exerciseCount: 14,
-    },
-  ]
 
   return (
     <div>
-      <Header name={courseName} />
+      <Header courseName={courseName} />
       <Content courseParts={courseParts} />
       <Total courseParts={courseParts} />
     </div>
