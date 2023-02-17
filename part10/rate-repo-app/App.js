4d29d0b221fb017d7929ffa5068618c095292f09
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-02-17 16:07:02 +0700

    add eslint config

--- part10/rate-repo-app/App.js
+++ part10/rate-repo-app/App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('logging to the console')
   return (
     <View style={styles.container}>
       <Text>Hey, I'm developing apps with React Native!</Text>
