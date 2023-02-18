26fff97ea9e039ff464c8b157f6b230e2f1669af
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-02-18 15:53:12 +0700

    implement a simple repository list

--- part10/rate-repo-app/App.js
+++ part10/rate-repo-app/App.js
@@ -1,21 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './src/components/Main'
 
-export default function App() {
-  console.log('logging to the console')
-  return (
-    <View style={styles.container}>
-      <Text>Hey, I'm developing apps with React Native!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () => {
+  return <Main />
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
