68544191633f61f11a8983bad3d84a96ce8ef5da
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-02-22 14:53:13 +0700

    implement simple signin form

--- part10/rate-repo-app/App.js
+++ part10/rate-repo-app/App.js
@@ -3,28 +3,17 @@ import { NativeRouter } from 'react-router-native'
 import { StatusBar } from 'expo-status-bar'
 
 import Main from './src/components/Main'
-// import CustomText, { FlexBox } from './playground'
-// import Text from './src/components/Text'
+// import BMICalculator from './playground/bmiCalculator'
+
 
 const App = () => {
   return (
     <>
+      {/* <BMICalculator /> */}
       <NativeRouter>
         <Main />
       </NativeRouter>
       <StatusBar style="auto" />
-      {/* <View>
-        <CustomText />
-        <View>
-          <Text>Simple text</Text>
-          <Text style={{ paddingBottom: 10 }}>Text with custom style</Text>
-          <Text fontWeight="bold" fontSize="subheading">
-            Bold subheading
-          </Text>
-          <Text color="textSecondary">Text with secondary color</Text>
-        </View>
-        <FlexBox />
-      </View> */}
     </>
   )
 }
--- part10/rate-repo-app/src/components/SignIn.jsx
+++ part10/rate-repo-app/src/components/SignIn.jsx
@@ -1,7 +1,51 @@
-import Text from './Text'
+import { Text, Button, TextInput } from 'react-native'
+import { Formik, useField } from 'formik'
 
+const FormikTextInput = ({ name, ...props }) => {
+  const [field, meta, helpers] = useField(name)
+  return (
+    <TextInput
+      name={field.name}
+      onChangeText={(text) => helpers.setValue(text)}
+      {...props}
+    />
+  )
+}
+
+const SignInForm = ({ handleSubmit }) => {
+  const [usernameField, usernameMeta, usernameHelpers] = useField('username')
+  const [pswdField, pswdMeta, pswdHelpers] = useField('password')
+  return (
+    <>
+      <TextInput
+        placeholder="Username"
+        name={usernameField.name}
+        onChangeText={(text) => usernameHelpers.setValue(text)}
+      ></TextInput>
+      <TextInput
+        placeholder="Password"
+        name={pswdField.name}
+        secureTextEntry
+        onChangeText={(text) => pswdHelpers.setValue(text)}
+      ></TextInput>
+      <Button title="Sign in" onPress={handleSubmit} />
+    </>
+  )
+}
+
+const initialValues = {
+  username: '',
+  password: '',
+}
 const SignIn = () => {
-  return <Text>Sign-in</Text>
+  const signin = (values) => {
+    console.log('signin/values', values)
+  }
+  return (
+    <Formik initialValues={initialValues} onSubmit={signin}>
+      {({ handleSubmit }) => <SignInForm handleSubmit={handleSubmit} />}
+    </Formik>
+  )
 }
 
 export default SignIn
